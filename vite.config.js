3ca57707a82c0f854b161c27c9bc5d28f83f03e7
Marco Antonio Ferreira Santos <ferreiras.marcoa@gmail.com>
2025-03-03 17:33:28 +0100

    ports added

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  server: {
+    port: process.env.PORT || 3000, // Usar el puerto definido por Render o el 3000 por defecto
+    host: '0.0.0.0', // Asegura que escuche en todas las interfaces de red
+  },
+};
